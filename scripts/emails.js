c2120c50855a4bd549d9847c5df088a843066b92
Yilin Ma <1135485226@qq.com>
2020-10-03 19:02:10 -0700

    Add SMTP credentials

--- scripts/emails.js
+++ scripts/emails.js
@@ -3,8 +3,8 @@ function sendEmail() {
     
     Email.send({
         Host: 'smtp.elasticemail.com',
-        Username: 'foo@email.com',
-        Password: 'you_credential',
+        Username: 'mayilin728@gmail.com',
+        Password: 'E93B2283BE1751EA56779B83027F47B720BA',
         To: emailAddr.value,
         From: 'mayilin728@gmail.com',
         Subject: 'Awesome Pictures from Yilin',
